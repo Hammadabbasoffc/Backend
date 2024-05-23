e464f27d031e2ce7ed5cd27421823f348f66f4c0
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-05-23 21:39:39 +0500

    On branch main Your branch is up to date with 'origin/main'.

--- scatch app/app.js
+++ scatch app/app.js
@@ -1,20 +1,24 @@
- const express = require('express');
- const app = express();
+const express = require("express");
+const app = express();
 
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
- const cookieParser = require('cookie-parser');
- const path = require('path');
+//  Routers
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
 
- const db = require("./config/mongoose.connection");
+const db = require("./config/mongoose.connection");
 
- app.use(express.json());
- app.use(express.urlencoded({extended: true}));
- app.use(cookieParser());
- app.use(express.static(path.join(__dirname, "public")));
- app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
- app.get("/", (req, res)=>{
-    res.send("Hello World");
- });
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
- app.listen(3000);
+app.listen(3000);
