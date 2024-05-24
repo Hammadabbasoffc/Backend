84b6142c5a65b30c24fb8a85a24621184541978a
Hammad Abbas <hammadabbasoffc@gmail.com>
2024-05-24 10:01:51 +0500

    On branch main Your branch is up to date with 'origin/main'.

--- scatch app/routes/ownersRouter.js
+++ scatch app/routes/ownersRouter.js
@@ -1,8 +1,36 @@
-const express = require('express')
+const express = require("express");
+require("dotenv").config();
+const ownerModel = require("../models/owner.model");
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.send("Hey its working")
-})
+
+
+router.get("/", (req, res) => {
+  res.send("Hey, it's working");
+});
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    try {
+      let owners = await ownerModel.find();
+
+      if (owners.length > 0) {
+        return res.status(503).send("You don't have permission to create Owner");
+      }
+
+      let { fullName, email, password } = req.body;
+      let createdOwner = await ownerModel.create({
+        fullName,
+        email,
+        password
+      });
+
+      res.status(201).send(createdOwner);
+    } catch (error) {
+      console.error("Error creating owner:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+}
 
 module.exports = router;
